03631ad154b30071171e2299dbb5094bb01b1f4f
Tomas <tomas@tomass-mbp.t-com.me>
2023-11-06 11:10:47 +0100

    pushing

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,13 @@
-export const fetchData = async (url, options) => {
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-              'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-              'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-            }
-          };
-    }
-}
-const response = await fetch(url, options);
-const data = await response.json();
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
 
-return data;
-}
+  return data;
+};
